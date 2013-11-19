d16216fa6752d6d88f961f13ff5d453eaa68e7a0
unknown <bturner_2@pibuk-lp71.pibenchmark.com>
2013-11-19 15:03:46 +0000

    add direct module prefix

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,23 @@
-#
-#  Can set up to 4 roles by setting the a custom Facter variable role_name1 to role_name4  
-#  e.g. before running puppet 
-#         export FACTER_role_name1=base
-#         export FACTER_role_name2=webserver
+#  If the prefix parameter is set then it will go directly to the module for the role.
+#  This allow a design pattern for modules where custom modules have a prefix (typically companyname) 
+#  This separates them from standard library modules downloaded from puppetforge which should not be customized.
+#  
+#  Otherwise it will call the class in the role module first. i.e. role::<role_name>
+#  
+#  For example:
+#   If prefix is set to mycompany 
+#        it will go directly to the mycompany-base  module and runthe init class
+#   Otherwise it will call the class role::base      
 #
 node default {
-  Exec {
+ Exec {
      path => ["/bin", "/sbin", "/usr/bin", "/usr/sbin"],
   }
-  class { 'role' : } 
-} 
+     class { 'role' }
+	# Or to do Direct Module Prefix 
+	#class { 'role' : prefix => 'mycompany' } 
+ } 
+
 
 
  
