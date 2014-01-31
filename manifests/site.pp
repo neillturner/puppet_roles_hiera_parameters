fb02bcd34d9a2e8f76a9e23fb0efa0ed8f4094b5
neillturner <neillwturner@gmail.com>
2014-01-31 12:20:16 +0000

    add full support for puppet master used hier data to specify roles. add support for environments.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,6 @@
+#
+#  Call Modules and Classes based on up to 4 roles 
+#
 #  If the prefix parameter is set then it will go directly to the module for the role.
 #  This allow a design pattern for modules where custom modules have a prefix (typically companyname) 
 #  This separates them from standard library modules downloaded from puppetforge which should not be customized.
@@ -13,7 +16,16 @@ node default {
  Exec {
      path => ["/bin", "/sbin", "/usr/bin", "/usr/sbin"],
   }
-     class { 'role' }
+     # check if roles defined in node hostname hiera file.
+     $hiera_role1 = hiera('role::role_name1','')
+     if $hiera_role1 != '' {
+        notify {"*** Found heira role::role_name1 value ${hiera_role1} ignoring all facter role values ***": }
+	    $role_name1 = hiera('role::role_name1','')
+        $role_name2 = hiera('role::role_name2','')
+        $role_name3 = hiera('role::role_name3','')
+  	    $role_name4 = hiera('role::role_name4','')
+	 }	
+     class { 'role': }
 	# Or to do Direct Module Prefix 
 	#class { 'role' : prefix => 'mycompany' } 
  } 
--- modules/role/manifests/init.pp
+++ modules/role/manifests/init.pp
@@ -1,70 +1,39 @@
 # == Class: role
 #
-# The roles can be either set on the individual server or on the puppetmaster master in the hieradata. 
+# The roles can be either set on the individual server or on the puppetmaster master in the hieradata for the node. 
 # If set in the puppetmaster hierdata this takes precedence. 
 #
+# SETTING THE ROLES
 # On the individual server set the Facter variable role_name1 to role_name4
+#   export FACTER_role_name1=base
+#   export FACTER_role_name2=webserver 
 # On the puppetmaster set the roles in the nodes\<hostname>.yaml file using values role::role_name1 to role::role_name4 
 #
+# MODULE CALLING 
 # This will result in either calls the role::role_nameX class
 # or
-#  if the prefix parameter is set then it will call directly the module for the role.
-# This allow a design pattern for modules where custom modules have a prefix (typically companyname) 
-# This separates them from standard library modules downloaded from puppetforge which should not be customized.
+#  if the prefix parameter is set then it will call directly the module for the role called prefix-role. 
 #
+# MODULE NAMING
+# There should be a separation of Library Modules from custom modules or classes.  
+# This allow a design pattern for modules where custom modules have a prefix (typically companyname) 
+# This separates them from standard library modules that can be specified using puppet librarian or downloaded 
+# from puppetforge or github which should then NOT be customized.
+#
+# ROLE ORDERING
 # We will always run the base role first but by default the other roles are run by puppet IN ANY ORDER.
 # If you want the other roles run add to the puppet command the option   --ordering=manifest 
 #  
-#
 # RESOVLING CLASS PARAMETERS 
-# the role class parameters can be resolved from the hier role/<role_name>.yaml file if the individual server set the Facter variable role_name1 to role_name4 
+# parameters for role can be set in the hier hierarchy.
+# They can be resolved from the hier <environment>/role/<role_name>.yaml and the role/<role_name>.yaml
+# This allows values to be set for the particular environment.
+# Also common parameters can be set in the common.yaml file which can also be specifed by environment.
 #
-class role( $prefix  = undef )
+class role($prefix  = undef  )
  { 
   include stdlib
-  $hiera_role1 = hiera('role::role_name1',nil)
-  if $hiera_role1 != nil {
-     notify {"*** Found heira value role::role_name1 ignoring client role values ***": }
-     $hiera_role2 = hiera('role::role_name2',nil)
-	 $hiera_role3 = hiera('role::role_name3',nil)
-	 $hiera_role4 = hiera('role::role_name4',nil)
-     if $prefix == undef {   
-        $role1 = "role::${hiera_role1}" 
-		if $hiera_role2 != nil { 
-		   $role2 = "role::$hiera_role2}"
-		} else {
-		   $role2 = ''
-		}
-		if $hiera_role3 != nil { 
-		   $role3 = "role::${hiera_role3}"
-		} else {
-		   $role3 = ''
-		}
-		if $hiera_role4 != nil { 
-		   $role4 = "role::${hiera_role4}"
-		} else {
-		   $role4 = ''
-		}		
-	 } else { 
-        $role1 = "${prefix}-${hiera_role1}"
-		if $hiera_role2 != nil { 
-		   $role2 = "${prefix}-${hiera_role2}"
-		} else {
-		   $role2 = ''
-		}
-		if $hiera_role3 != nil { 
-		   $role3 = "${prefix}-${hiera_role3}"
-		} else {
-		   $role3 = ''
-		}
-		if $hiera_role4 != nil { 
-		   $role4 = "${prefix}-${hiera_role4}"
-		} else {
-		   $role4 = ''
-		}				
-	 }
-  } else {  	 
-  if $role_name1 != '' and $prefix == undef {
+-  if $role_name1 != '' and $prefix == undef {
      $role1 = "role::${role_name1}"
    } elsif $role_name1 != '' {
       $role1 = "${prefix}-${role_name1}"
@@ -92,7 +61,6 @@ class role( $prefix  = undef )
    } else { 
  	 $role4 = '' 
   }
-  }  
   $roles = [ $role1, $role2, $role3, $role4]
   if $prefix  != undef {  
      notify {"*** Direct Module Prefix ${prefix} ***": }
